5d1a33383663d967163069f289a3033828039199
Gustavo Paco <gustavopaco@gmail.com>
2023-09-23 09:07:36 -0300

    🚀 Adicionando componentes e módulos de Material UI para o Navbar e Layout
    - Adicionando o `MatToolbarModule`, `MatIconModule`, `MatButtonModule`, `MatSidenavModule` e `MatListModule` no `imports` do `NavbarComponent` e `LayoutComponent`.
    - Adicionando botão de menu no `NavbarComponent` para abrir o `MatSidenav`.
    - Adicionando links para "Portfolio" e "Logout" no `NavbarComponent`.
    - Adicionando o `MatSidenavContainer`, `MatSidenav` e `MatSidenavContent` no `LayoutComponent`.
    - Adicionando conteúdo do menu no `MatSidenav`.
    - Adicionando conteúdo principal na `router-outlet` do `MatSidenavContent`.
    - Atualizando o estilo do `NavbarComponent` e `LayoutComponent` para ajustar a altura.
    - Adicionando classes de estilo para o `LayoutComponent

--- src/app/containers/layout/layout.component.ts
+++ src/app/containers/layout/layout.component.ts
@@ -5,11 +5,17 @@ import {RouterOutlet} from "@angular/router";
 import {FooterComponent} from "../footer/footer.component";
 import {UiService} from "../../shared/services/default/ui.service";
 import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import {BreakpointObserver} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, RouterOutlet, FooterComponent],
+  imports: [CommonModule, NavbarComponent, RouterOutlet, FooterComponent, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatListModule],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
@@ -17,17 +23,44 @@ export class LayoutComponent {
 
   isShowNavbar = true;
   isShowFooter = true;
+  isShowSideNav = false;
 
-  constructor(private uiService: UiService) {
+  isLgScreen: boolean = false;
+
+  constructor(private uiService: UiService,
+              private breakPointObserver: BreakpointObserver) {
     this.uiService.isShowNavBarEventEmitter
       .pipe(takeUntilDestroyed())
       .subscribe((value) => {
-      this.isShowNavbar = value;
-    });
+        this.isShowNavbar = value;
+      });
     this.uiService.isShowFooterEventEmitter
       .pipe(takeUntilDestroyed())
       .subscribe((value) => {
-      this.isShowFooter = value;
-    });
+        this.isShowFooter = value;
+      });
+    this.breakPointObserver.observe(['(min-width: 768px)'])
+      .pipe(takeUntilDestroyed())
+      .subscribe(result => {
+        this.isLgScreen = result.breakpoints['(min-width: 768px)'];
+      })
+  }
+
+  toggleSideNav(sideNav: boolean) {
+    this.isShowSideNav = sideNav;
+  }
+
+  public layoutClassStyle(): string {
+    if (!this.isShowNavbar && !this.isShowFooter) {
+      return 'incomplete-layout';
+    }
+    if ((this.isShowNavbar && !this.isShowFooter) || (!this.isShowNavbar && this.isShowFooter)) {
+      return 'parcial-layout';
+    }
+    return 'complete-layout';
+  }
+
+  onGoTo($event: any) {
+    console.log('Movendo para: ', $event);
   }
 }
--- src/app/containers/navbar/navbar.component.ts
+++ src/app/containers/navbar/navbar.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  @Input('lgScreen') isLgScreen: boolean = false;
+  @Output() sideNav = new EventEmitter(false);
+  @Output() goTo = new EventEmitter(false);
+  isShowSideNav = false;
+
+  onGoTo(path: string) {
+    this.goTo.emit(path);
+  }
+
+  onSideNav() {
+    this.isShowSideNav = !this.isShowSideNav;
+    this.sideNav.emit(this.isShowSideNav);
+  }
 }

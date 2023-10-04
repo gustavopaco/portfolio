3b8643e38c8be07914469147a04b1ee0b621a8f7
Gustavo Paco <gustavopaco@gmail.com>
2023-10-03 23:39:26 -0300

    :sparkles: Exclusão de imagem do bucket da Aws S3 ao remover skill.
    :sparkles: Exclusão de Skill do Banco de dados.
    
    ✨🐛 Corrige bug de exclusão de habilidades
    
    Corrige um bug que impedia a exclusão correta de habilidades. Agora, ao
    excluir uma habilidade, a mesma é removida corretamente da lista de
    habilidades exibida.

--- src/app/components/skills/containers/skills/skills.component.ts
+++ src/app/components/skills/containers/skills/skills.component.ts
@@ -11,6 +11,14 @@ import {SkillsListComponent} from "../../components/skill-list/skills-list.compo
 import {HttpValidator} from "../../../../shared/validator/http-validator";
 import {UserService} from "../../../../shared/services/user.service";
 import {MatSnackbarService} from "../../../../shared/external/angular-material/toast-snackbar/mat-snackbar.service";
+import {
+  ConfirmationDialogComponent
+} from "../../../../shared/external/angular-material/confirmation-dialog/confirmation-dialog.component";
+import {TranslateService} from "@ngx-translate/core";
+import {CredentialsService} from "../../../../shared/services/credentials.service";
+import {UtilAwsS3Service} from "../../../../shared/services/default/aws/util-aws-s3.service";
+import {AwsConfiguration} from "../../../../shared/interface/aws-configuration";
+import {ACTION_CLOSE, FAILED_TO_DELETE_SKILL, SKILL_DELETED_SUCCESSFULLY} from "../../../../shared/constants/constants";
 
 @Component({
   selector: 'app-skills',
@@ -31,7 +39,10 @@ export class SkillsComponent implements OnInit {
 
   constructor(private matDialog: MatDialog,
               private userService: UserService,
-              private matSnackBarService: MatSnackbarService) {
+              private matSnackBarService: MatSnackbarService,
+              private translateService: TranslateService,
+              private credentialsService: CredentialsService,
+              private utilAwsS3Service: UtilAwsS3Service) {
   }
 
   ngOnInit(): void {
@@ -68,7 +79,67 @@ export class SkillsComponent implements OnInit {
   }
 
   onDeleteSkill(skillId: number) {
-    console.log("Chamou o delete para o id: " + skillId)
+    this.openConfirmDeleteDialog(skillId);
+  }
+
+  private openConfirmDeleteDialog(skillId: number) {
+    const dialogRef = this.matDialog.open(ConfirmationDialogComponent, {
+      width: '100%',
+      height: 'auto',
+      maxWidth: '400px',
+      maxHeight: '700px',
+      disableClose: true,
+      data: {
+        title: this.translateService.instant('deleteSkillTitle'),
+        message: this.translateService.instant('deleteSkillMessage', {skillName: this.filterSkillById(skillId).name}),
+        btnConfirmLabel: this.translateService.instant('deleteButton'),
+        confirmColor: 'warn',
+        confirmIcon: 'delete',
+        btnCancelLabel: this.translateService.instant('cancelButton'),
+        cancelColor: 'primary'
+      }
+    });
+
+    dialogRef.afterClosed()
+      .pipe(take(1))
+      .subscribe((result: boolean) => {
+        if (result) {
+          this.loadCredentials(skillId);
+        }
+      });
+  }
+
+  private loadCredentials(skillId: number) {
+    this.credentialsService.getAwsCredentials()
+      .pipe(take(1))
+      .subscribe({
+        next: (response: AwsConfiguration) => this.deleteSkillFromAws(skillId, response),
+        error: (error) => this.matSnackBarService.error(HttpValidator.validateResponseErrorMessage(error), 'Failed', 5000)
+      })
+  }
+
+  private deleteSkillFromAws(skillId: number, response: AwsConfiguration) {
+    this.utilAwsS3Service.loadS3Client(response.region, response.accessKey, response.secretKey);
+    this.utilAwsS3Service.deleteImageFromAwsS3Bucket(response.bucketName, this.filterSkillById(skillId).url)
+      .then(() => this.deleteSkill(skillId))
+      .catch(() => this.matSnackBarService.error(FAILED_TO_DELETE_SKILL, ACTION_CLOSE, 5000))
+  }
+
+  private deleteSkill(skillId: number) {
+    this.userService.deleteSkillRecord(skillId)
+      .pipe(take(1))
+      .subscribe({
+        next: () => {
+          this.matSnackBarService.success(SKILL_DELETED_SUCCESSFULLY, ACTION_CLOSE, 5000);
+          this.removeSkillFromList(skillId);
+          this.resetSkillSelected();
+        },
+        error: (error) => this.matSnackBarService.error(HttpValidator.validateResponseErrorMessage(error), 'Failed', 5000)
+      })
+  }
+
+  private removeSkillFromList(skillId: number) {
+    this.skills = this.skills.filter(skill => skill.id !== skillId);
   }
 
   private openSkillFormDialog(newSkillResult: boolean, skillOnEdit?: Skill) {

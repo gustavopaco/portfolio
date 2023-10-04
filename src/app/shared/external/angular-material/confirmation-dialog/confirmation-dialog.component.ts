53ffafd524c890c1a7b4c0550296b1225970a762
Gustavo Paco <gustavopaco@gmail.com>
2023-10-03 23:33:37 -0300

    🎨 Adiciona ícones e cores aos botões de confirmação e cancelamento
    - Adiciona a propriedade `confirmColor` ao objeto `DialogData` para definir a cor do botão de confirmação.
    - Adiciona a propriedade `confirmIcon` ao objeto `DialogData` para exibir um ícone no botão de confirmação.
    - Adiciona a propriedade `cancelColor` ao objeto `DialogData` para definir a cor do botão de cancelamento.
    - Adiciona a propriedade `cancelIcon` ao objeto `DialogData` para exibir um ícone no botão de cancelamento.
    
    Resolves: #123

--- src/app/shared/external/angular-material/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/shared/external/angular-material/confirmation-dialog/confirmation-dialog.component.ts
@@ -2,18 +2,24 @@ import {Component, Inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
 import {MatButtonModule} from "@angular/material/button";
+import {ThemePalette} from "@angular/material/core";
+import {MatIconModule} from "@angular/material/icon";
 
 export interface DialogData {
   title: string;
   message: string;
   btnConfirmLabel: string ;
+  confirmColor: ThemePalette;
+  confirmIcon: string;
   btnCancelLabel: string;
+  cancelColor: ThemePalette;
+  cancelIcon: string;
 }
 
 @Component({
   selector: 'app-confirmation-dialog',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  imports: [CommonModule, MatDialogModule, MatButtonModule, MatIconModule],
   templateUrl: './confirmation-dialog.component.html',
   styleUrls: ['./confirmation-dialog.component.scss']
 })

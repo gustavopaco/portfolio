06febceb321fe8d61a1751082db7e6705fb330f9
Gustavo Paco <gustavopaco@gmail.com>
2023-11-02 23:37:43 -0300

    :sparkles: Adicionado Lib Swiper ao projeto

---
+++ src/app/shared/diretivas/swiper.directive.ts
@@ -0,0 +1,19 @@
+import {AfterViewInit, Directive, ElementRef, Input} from '@angular/core';
+import {SwiperOptions} from "swiper/types";
+import {SwiperContainer} from "swiper/swiper-element";
+
+@Directive({
+  selector: '[swiper]',
+  standalone: true
+})
+export class SwiperDirective implements AfterViewInit {
+  @Input() config?: SwiperOptions
+
+  constructor(private el: ElementRef<SwiperContainer>) {}
+
+  ngAfterViewInit(): void {
+    Object.assign(this.el.nativeElement, this.config);
+
+    this.el.nativeElement.initialize();
+  }
+}

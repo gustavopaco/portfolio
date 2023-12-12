e01999620c47570a4308eab7a4c87a938600ff53
Gustavo Paco <gustavopaco@gmail.com>
2023-12-11 23:46:39 -0300

    :lipstick: Alterando aparecencia de drop-zone
    file-uploader.component.scss
    file-uploader.component.html

--- src/app/shared/external/angular-material/file-uploader/file-uploader.component.ts
+++ src/app/shared/external/angular-material/file-uploader/file-uploader.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {DragAndDropDirective} from "../../../diretivas/drag-and-drop.directive";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-file-uploader',
   standalone: true,
-  imports: [CommonModule, DragAndDropDirective],
+  imports: [CommonModule, DragAndDropDirective, MatIconModule],
   templateUrl: './file-uploader.component.html',
   styleUrl: './file-uploader.component.scss'
 })

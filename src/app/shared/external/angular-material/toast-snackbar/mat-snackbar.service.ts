220dc89e1fea8dca95cee3e87e20cdfbe2be9fe1
Gustavo Paco <gustavopaco@gmail.com>
2023-12-16 22:32:55 -0300

    :recycle: Adicionado possibilidade para envio de multiplos snackbar
    mat-snackbar.service.ts

--- src/app/shared/external/angular-material/toast-snackbar/mat-snackbar.service.ts
+++ src/app/shared/external/angular-material/toast-snackbar/mat-snackbar.service.ts
@@ -1,50 +1,57 @@
-import {Injectable} from '@angular/core';
+import {Injectable, OnDestroy} from '@angular/core';
 import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from "@angular/material/snack-bar";
 
 @Injectable({
   providedIn: 'root'
 })
-export class MatSnackbarService {
+export class MatSnackbarService implements OnDestroy {
   private panelClass: string[] = ['toast-success'];
   private action = 'Fechar';
   private durationInSeconds = 3000;
   private horizontalPosition: MatSnackBarHorizontalPosition = 'end';
   private verticalPosition: MatSnackBarVerticalPosition = 'top';
+  private timeouts: NodeJS.Timeout[] = [];
 
   constructor(private snakeBar: MatSnackBar) {
   }
 
   success(message: string, action?: string, duration?: number, horizontalPosition?: string, verticalPosition?: string) {
-    this.panelClass = ['success'];
-    this.configureSnakeBar(action, duration, horizontalPosition, verticalPosition);
-    this.openSnackBar(message);
+    const panelClass = ['success'];
+    const config = this.configureSnakeBar(panelClass, duration, horizontalPosition, verticalPosition);
+    this.openSnackBar(message, config, action);
   }
 
   error(message: string, action?: string, duration?: number, horizontalPosition?: string, verticalPosition?: string) {
-    this.panelClass = ['error'];
-    this.configureSnakeBar(action, duration, horizontalPosition, verticalPosition);
-    this.openSnackBar(message);
+    const panelClass = ['error'];
+    const config = this.configureSnakeBar(panelClass, duration, horizontalPosition, verticalPosition);
+    this.openSnackBar(message, config, action);
   }
 
   warning(message: string, action?: string, duration?: number, horizontalPosition?: string, verticalPosition?: string) {
-    this.panelClass = ['warning'];
-    this.configureSnakeBar(action, duration, horizontalPosition, verticalPosition);
-    this.openSnackBar(message);
+    const panelClass = ['warning'];
+    const config = this.configureSnakeBar(panelClass, duration, horizontalPosition, verticalPosition);
+    this.openSnackBar(message, config, action);
   }
 
-  configureSnakeBar(action?: string, duration?: number, horizontalPosition?: string, verticalPosition?: string) {
-    this.action = action ? action : this.action;
-    this.durationInSeconds = duration ? duration : this.durationInSeconds;
-    this.horizontalPosition = horizontalPosition ? horizontalPosition as MatSnackBarHorizontalPosition : this.horizontalPosition;
-    this.verticalPosition = verticalPosition ? verticalPosition as MatSnackBarVerticalPosition : this.verticalPosition;
+  configureSnakeBar(panelClass?: string[], duration?: number, horizontalPosition?: string, verticalPosition?: string) {
+    return {
+      duration: duration ?? this.durationInSeconds,
+      panelClass: panelClass ?? this.panelClass,
+      horizontalPosition: horizontalPosition as MatSnackBarHorizontalPosition ?? this.horizontalPosition,
+      verticalPosition: verticalPosition as MatSnackBarVerticalPosition ?? this.verticalPosition,
+    }
   }
 
-  openSnackBar(message: string) {
-    this.snakeBar.open(message, this.action, {
-      duration: this.durationInSeconds,
-      panelClass: this.panelClass,
-      horizontalPosition: this.horizontalPosition,
-      verticalPosition: this.verticalPosition,
-    });
+  openSnackBar(message: string, config: any, action?: string) {
+    const time = setTimeout(() => {
+      action = action ?? this.action;
+      this.snakeBar.open(message, action, config);
+    }, config.duration);
+    this.timeouts.push(time);
   }
+
+  ngOnDestroy(): void {
+    this.timeouts.forEach(timeout => clearTimeout(timeout));
+  }
+
 }

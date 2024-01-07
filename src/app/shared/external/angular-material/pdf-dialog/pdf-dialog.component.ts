ac3613809c619e5ce732edc2fbef17f44a7e7aff
Gustavo Paco <gustavopaco@gmail.com>
2024-01-06 23:05:32 -0300

    :recycle: Alterando comportamento de componente de PDF para que possa ser exibido multiplos arquivos no mesmo componente.
    pdf-dialog.component.html
    pdf-dialog.component.ts

--- src/app/shared/external/angular-material/pdf-dialog/pdf-dialog.component.ts
+++ src/app/shared/external/angular-material/pdf-dialog/pdf-dialog.component.ts
@@ -1,6 +1,11 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MAT_DIALOG_DATA, MatDialogContent} from "@angular/material/dialog";
+
+export interface PdfDialogData {
+  url: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-pdf-dialog',
@@ -9,14 +14,56 @@ import {MAT_DIALOG_DATA, MatDialogContent} from "@angular/material/dialog";
   templateUrl: './pdf-dialog.component.html',
   styleUrl: './pdf-dialog.component.scss'
 })
-export class PdfDialogComponent {
+export class PdfDialogComponent implements OnInit {
 
   iframeSrc: string = '';
   title: string = '';
+  position: any;
+  totalElements: any;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
-    this.iframeSrc = data.url;
-    this.title = data.title;
-    console.log('iframeSrc', this.iframeSrc);
+  constructor(@Inject(MAT_DIALOG_DATA) public data: PdfDialogData[]) {
+    this.position = 0;
+    this.totalElements = data.length;
+  }
+
+  ngOnInit(): void {
+    this.start();
+  }
+
+  start() {
+    this.iframeSrc = this.data[this.position].url;
+    this.title = this.data[this.position].title;
+  }
+
+  next() {
+    if (this.position < this.totalElements - 1) {
+      this.position++;
+      this.iframeSrc = this.data[this.position].url;
+      this.title = this.data[this.position].title;
+    }
+  }
+
+  previous() {
+    if (this.position > 0) {
+      this.position--;
+      this.iframeSrc = this.data[this.position].url;
+      this.title = this.data[this.position].title;
+    }
+  }
+
+  isFirst() {
+    return this.position === 0;
+  }
+
+  isLast() {
+    return this.position === this.totalElements - 1;
+  }
+
+  isSingle() {
+    return this.totalElements === 1;
+  }
+
+  isMultiple() {
+    return this.totalElements > 1;
   }
 }

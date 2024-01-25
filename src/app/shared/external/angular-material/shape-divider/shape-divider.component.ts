1251c221e7a9db56a6dade45fd988298e3da3b91
Gustavo Paco <gustavopaco@gmail.com>
2024-01-25 10:59:37 -0300

    :sparkles: Adicionado novos modos de shapes divider.
    :sparkles: Adicionado funcionalidade de 'flip/girar' ao shape-divider
    
    :recycle: Refatorado todo o código do componente shape-divider para melhorar manutenção.
    
    shape-divider.component.ts
    shape-divider.component.html
    shape-divider.component.scss
    
    :sparkles: Adicionado Service para cuidar de métodos de configuração do componente shape-divider.
    
    shape-divider.service.ts

--- src/app/shared/external/angular-material/shape-divider/shape-divider.component.ts
+++ src/app/shared/external/angular-material/shape-divider/shape-divider.component.ts
@@ -1,5 +1,11 @@
 import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
+import {ShapeDividerService} from "./shape-divider.service";
+
+export interface ShapeDividerPath {
+  d: string;
+  opacity: number;
+}
 
 @Component({
   selector: 'app-shape-divider',
@@ -8,16 +14,27 @@ import { CommonModule } from '@angular/common';
   templateUrl: './shape-divider.component.html',
   styleUrl: './shape-divider.component.scss'
 })
-export class ShapeDividerComponent implements AfterViewInit  {
+export class ShapeDividerComponent implements OnInit, AfterViewInit {
 
+  @Input() shape: 'waves' | 'waves-opacity' | 'curve' | 'curve-asymmetrical' | 'triangule' | 'triangule-asymmetrical' | 'tilt' | 'arrow' | 'split' | 'book' = 'waves';
   @Input() baseHeight: number = 50;
   @Input() svgHeight: number = 150;
   @Input() svgWidth: number = 100;
   @Input() bottom: boolean = false;
+  @Input() flip: boolean = false;
   @Input() inverted: boolean = false;
   @Input() hexaColor: string = '#ffffff';
   @ViewChild('shapeDivider') shapeDivider!: ElementRef;
   @ViewChild('svgDivider') svgDivider!: ElementRef;
+
+  paths: ShapeDividerPath[] = [];
+
+  constructor(private shapeDividerService: ShapeDividerService) {
+  }
+
+  ngOnInit(): void {
+    this.paths = this.getPaths();
+  }
 
   ngAfterViewInit(): void {
     if (this.bottom) {
@@ -28,5 +45,32 @@ export class ShapeDividerComponent implements AfterViewInit  {
     this.svgDivider.nativeElement.style.width = `calc(${this.svgWidth}% + 1.3px)`; // 1.3px is the border width
   }
 
+  setShapeDividerNgClass() {
+    switch (this.shape) {
+      case 'waves-opacity' || 'tilt':
+        return {
+          'bottom-divider inverted': this.bottom,
+        };
+      default:
+        return {
+          'bottom-divider': this.bottom,
+          'inverted': (this.bottom && !this.inverted) || (!this.bottom && this.inverted),
+        };
+    }
+  }
 
+  setSvgDividerNgClass() {
+    switch (this.shape) {
+      case 'waves' || 'waves-opacity' || 'curve-asymmetrical'  || 'triangule-asymmetrical' || 'tilt':
+        return {
+          'flip': this.flip,
+        };
+      default:
+        return {};
+    }
+  }
+
+  getPaths(): ShapeDividerPath[] {
+    return this.shapeDividerService.getShapeDividerPath(this.shape, this.inverted);
+  }
 }

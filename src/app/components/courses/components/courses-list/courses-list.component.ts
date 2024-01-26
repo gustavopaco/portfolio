0fa5372e0e2cd440944d08f885c7c1bf1675572e
Gustavo Paco <gustavopaco@gmail.com>
2024-01-26 16:51:50 -0300

    :bug: :lipstick: Correção de um pequeno bug de posicionamento de elemento na listagem de cursos.
    courses-list.component.html
    courses-list.component.scss

--- src/app/components/courses/components/courses-list/courses-list.component.ts
+++ src/app/components/courses/components/courses-list/courses-list.component.ts
@@ -1,14 +1,13 @@
 import {Component, Input} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {Course} from "../../../../shared/interface/course";
-import {MatIconModule} from "@angular/material/icon";
 import {TranslateModule} from "@ngx-translate/core";
 import {MatCardModule} from "@angular/material/card";
 
 @Component({
   selector: 'app-courses-list',
   standalone: true,
-  imports: [CommonModule, MatIconModule, TranslateModule, MatCardModule],
+  imports: [CommonModule, TranslateModule, MatCardModule],
   templateUrl: './courses-list.component.html',
   styleUrl: './courses-list.component.scss'
 })
